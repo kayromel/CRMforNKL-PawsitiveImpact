ed1221f7e5261b4df76dea3b4e4a43236d1c95f1
Jacob Riggs <jarigg@iu.edu>
2024-01-02 17:22:21 -0500

    This better work

---
+++ NoKill-Inventory/No-Kill-Inventory/No-Kill-Inventory/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace No_Kill_Inventory.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext<ApplicationUser>(options)
+{
+}
--- NoKill-Inventory/NoKill-Inventory/Data/ItemContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace NoKill_Inventory.Data;
-
-public class ItemContext : DbContext
-{
-    public DbSet<Item> Inventory { get; set; }
-}
