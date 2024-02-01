71ab13d808fb9035eac53006ee2318a057abf229
Jacob Riggs <jarigg@iu.edu>
2024-02-01 13:39:34 -0500

    Added button code to get around merge conflict from other commits

--- NoKill-Inventory/No-Kill-Inventory/No-Kill-Inventory/Data/ApplicationDbContext.cs
+++ NoKill-Inventory/No-Kill-Inventory/No-Kill-Inventory/Data/ApplicationDbContext.cs
@@ -6,5 +6,10 @@ namespace No_Kill_Inventory.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : IdentityDbContext<ApplicationUser>(options)
 {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=data/app.db");
+    }
+    
     public DbSet<Item> Items { get; set; }
 }
