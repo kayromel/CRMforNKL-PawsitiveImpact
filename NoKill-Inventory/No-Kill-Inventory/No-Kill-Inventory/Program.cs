9d6f394257dc8f78635af505700fb930167033c5
elizabethmittel <ebethmittel03@gmail.com>
2024-02-27 17:05:16 -0500

    Inventory Entry Pages Complete

--- NoKill-Inventory/No-Kill-Inventory/No-Kill-Inventory/Program.cs
+++ NoKill-Inventory/No-Kill-Inventory/No-Kill-Inventory/Program.cs
@@ -37,6 +37,8 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
+builder.Services.AddServerSideBlazor().AddCircuitOptions(options => { options.DetailedErrors = true; });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
