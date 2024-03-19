c57fc227c8ec030253c7bd935c576d25586a694f
Jacob Riggs <jacobriggs914@gmail.com>
2024-03-19 16:51:17 -0400

    Added max length to other item

---
+++ NoKill-Inventory/No-Kill-Inventory/No-Kill-Inventory/Data/Migrations/20240319203016_Lengths.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace No_Kill_Inventory.Migrations
+{
+    /// <inheritdoc />
+    public partial class Lengths : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "b74ddd14-6340-4840-95c2-db12554843e5",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "2ab830ef-168b-4b06-8e81-32e994b6c940", "AQAAAAIAAYagAAAAEIaOXzntZdCK5dGWUtXeYu5gOU8fFfJs86/HlR5WNP/EOk431crSJRdyQuESnVIdPA==", "f22202e5-051d-4382-bc53-7b440341e006" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "b74ddd14-6340-4840-95c2-db12554843e5",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "b83bd6ff-d0f6-4bc1-8174-a0d677488ca0", "AQAAAAIAAYagAAAAEDkeBrjsTK3Zi+7EID4KQHJW+s/j02Km3OwmzmoNwG7Mq8UySVIwC07F2jFF9Aeo4Q==", "487e0faf-3bb3-4b60-85d3-d3af35a9bed4" });
+        }
+    }
+}
--- NoKill-Inventory/No-Kill-Inventory/No-Kill-Inventory/Data/OtherItem.cs
+++ NoKill-Inventory/No-Kill-Inventory/No-Kill-Inventory/Data/OtherItem.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.Runtime.InteropServices.JavaScript;
+using Microsoft.AspNetCore.Components;
 
 namespace No_Kill_Inventory.Data;
 
@@ -8,8 +9,10 @@ public class OtherItem
     [Key]
     public int ItemID { get; set; }
     
+    [StringLength(100)]
     public String Animal { get; set; }
     
+    [StringLength(500)]
     public String Desc { get; set; }
     
     public int Weight { get; set; }
