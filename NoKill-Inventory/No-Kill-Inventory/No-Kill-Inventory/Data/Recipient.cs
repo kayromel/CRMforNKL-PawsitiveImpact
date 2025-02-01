6a0322e38fe20adf2a855e689cb693ed07a80a93
doranmartin <doranmartin1994@gmail.com>
2025-02-01 12:32:59 -0500

    Adding Pet functionality is there, needs work

--- NoKill-Inventory/No-Kill-Inventory/No-Kill-Inventory/Data/Recipient.cs
+++ NoKill-Inventory/No-Kill-Inventory/No-Kill-Inventory/Data/Recipient.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace No_Kill_Inventory.Data;
 public class Recipient
 {
     [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     
     public String FirstName { get; set; }
